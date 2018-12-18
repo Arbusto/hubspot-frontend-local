7d61211e338a67a934f21ccc9a983be78069b445
Augusto Schiavi <A.Schiavi@gmail.com>
2018-12-18 17:49:31 -0300

    Added news page and unminified

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 require('bootstrap');
+require('jquery');
+
+window.moreEbooks = () => {
+  const viewMoreBtn = $('.library > .section-2').find('.buttons-bottom');
+  const viewMoreBtnRow = viewMoreBtn.closest('.row');
+  const extraContent = $('.library > .section-2').find('.extra-content');
+  viewMoreBtnRow.hide();
+  extraContent.show();
+}
+
+window.moreSucessStories = () => {
+  const viewMoreBtn = $('.library > .section-3').find('.buttons-bottom');
+  const viewMoreBtnRow = viewMoreBtn.closest('.row');
+  const extraContent = $('.library > .section-3').find('.extra-content');
+  viewMoreBtnRow.hide();
+  extraContent.show();
+}
